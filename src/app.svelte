9530fba12f287fc91ecdc96aa24f81801b801dbc
Wira Maharddhika <wiramaharddhika@gmail.com>
2021-02-16 12:43:18 +0800

    init

---
+++ src/app.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing'
+  import Home from './pages/home.svelte'
+  import Detail from './pages/detail.svelte'
+</script>
+
+<Router>
+  <Route path="/:id">
+    <Detail />
+  </Route>
+  <Route path="/">
+    <Home />
+  </Route>
+</Router>
